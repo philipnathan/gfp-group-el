5be36054afc4b4b023331cc08eed225de609677a
Frisqia <frifani123@gmail.com>
2024-07-31 02:25:03 +0700

    creat filter to searching

--- Frontend/my-nextjs-app/src/app/pages/Dashboard/navbar/searchbar.tsx
+++ Frontend/my-nextjs-app/src/app/pages/Dashboard/navbar/searchbar.tsx
@@ -1,7 +1,13 @@
 'use client';
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
+import FilterSearch from "./filter";
 
-export default function SearchNav() {
+interface SearchNavProps {
+  isFilterMenuOpen: boolean;
+  toggleFilterMenu: () => void;
+}
+
+export default function SearchNav({ isFilterMenuOpen, toggleFilterMenu }: SearchNavProps) {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const searchValue = (e.target as HTMLFormElement).search.value;
@@ -17,10 +23,22 @@ export default function SearchNav() {
           name="search"
           className="search-input p-2 border border-gray-300 rounded"
         />
-        <button type="submit" className="search-button ml-2 bg-custom-green text-white px-4 py-2 rounded">
-        <i className="fa fa-search"></i>
+        <button type="submit" className="search-button ml-2 bg-custom-green text-white px-4 py-2 rounded hover:bg-custom-green/80 transition duration-300">
+          <i className="fa fa-search"></i>
+        </button>
+        <button
+          type="button"
+          className="search-button text-custom-green px-4 py-2 text-5xl hover:text-custom-green/80 transition duration-300"
+          onClick={toggleFilterMenu}
+        >
+          <i className="fa fa-sliders"></i>
         </button>
       </form>
+      {isFilterMenuOpen && (
+        <div className="flex flex-col items-center space-x-4 mt-4 overflow-y-auto max-h-64">
+          <FilterSearch />
+        </div>
+      )}
     </div>
   );
 }
