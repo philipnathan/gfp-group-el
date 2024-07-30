b5fe9b43902b43bb768b87d6af69b5aec1c61bfe
Frisqia <frifani123@gmail.com>
2024-07-30 20:15:54 +0700

    installation package

---
+++ Frontend/my-nextjs-app/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+     "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-green': '#206900',
+        'custom-Olive-Drab': '#597226',
+        'custom-pastel-green': '#cadcbf',
+        'custom-light-over-green': '#93bc3f',
+        'custom-steel-blue' : '#4c8da8',
+        'custom-Gunmetal' : '#242c34',
+        'custom-soft-blue' : '#74b3d4'
+      },
+      // backgroundImage:{
+      //   'custom-backround': "url('/baground.png')",
+      // }
+    },
+  },
+  plugins: [],
+}
