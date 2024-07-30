89ee84f9bf958e32892bf89b4acdf4a4571a84d9
Frisqia <frifani123@gmail.com>
2024-07-30 20:19:15 +0700

    footer and social media button

---
+++ Frontend/my-nextjs-app/src/app/pages/Dashboard/footer/sosialmedia.tsx
@@ -0,0 +1,24 @@
+import "../../../globals.css";
+
+
+export default function SosialMedia(){
+    return(
+        <>
+        <div className="bg-white p-10 text-white">
+        <div className="grid justify-center text-center bg-custom-steel-blue p-10 rounded-lg  ">
+        <h1 className="text-4xl">Our Social Media</h1>
+        <p>Join us and follow to get latest products, offers and innovative ideas on Made From Recycled</p>
+        <div className="flex justify-between p-5">
+        <a href="#" className="fa fa-facebook p-5 text-white border border-white border-4 rounded-full transition-transform duration-500 hover:scale-105"></a>
+        <a href="#" className="fa fa-twitter p-5 text-white border border-white border-4 rounded-full transition-transform duration-500 hover:scale-105"></a>
+        <a href="#" className="fa fa-google p-5 text-white border border-white border-4 rounded-full transition-transform duration-500 hover:scale-105"></a>
+        <a href="#" className="fa fa-linkedin p-5 text-white border border-white border-4 rounded-full transition-transform duration-500 hover:scale-105"></a>
+        <a href="#" className="fa fa-youtube p-5 text-white border border-white border-4 rounded-full transition-transform duration-500 hover:scale-105"></a>
+        <a href="#" className="fa fa-instagram p-5 text-white border border-white border-4 rounded-full transition-transform duration-500 hover:scale-105"></a>
+        </div>
+        </div>
+        </div>
+        </>
+   
+    )
+}
