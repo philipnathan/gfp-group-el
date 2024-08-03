5f64280321bf4658db60c38c57474bca07999d75
philipnathan <philipnathan168@gmail.com>
2024-08-03 10:47:36 +0800

    change: search logic from prov->subd, to, subdist->prov

--- backend/app/controllers/locations/locations_controller.py
+++ backend/app/controllers/locations/locations_controller.py
@@ -1,4 +1,5 @@
 from flask import request
+from flasgger import swag_from
 
 from . import locations_blueprint
 from .locations_services import LocationServices
@@ -8,21 +9,25 @@ service = LocationServices()
 
 
 @locations_blueprint.route("/provinces", methods=["GET"])
+@swag_from("./location_all_provinces.yml")
 def get_provinces():
     return service.get_provinces()
 
 
 @locations_blueprint.route("/districts", methods=["GET"])
+@swag_from("./location_all_districts.yml")
 def get_districts():
     return service.get_districts()
 
 
 @locations_blueprint.route("/subdistricts", methods=["GET"])
+@swag_from("./location_all_subdistricts.yml")
 def get_subdistricts():
     return service.get_subdistricts()
 
 
 @locations_blueprint.route("/search", methods=["GET"])
+@swag_from("./location_by_id.yml")
 def get_location():
     prov_id = request.args.get("prov_id", type=int)
     dist_id = request.args.get("dist_id", type=int)
