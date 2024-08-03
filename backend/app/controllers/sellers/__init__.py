0052df8c8612216cd7077cd35b23a41df95189c1
philipnathan <philipnathan168@gmail.com>
2024-08-03 10:55:08 +0800

    add: add prefix /api/ front of all blueprints

--- backend/app/controllers/sellers/__init__.py
+++ backend/app/controllers/sellers/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-sellers_blueprint = Blueprint("sellers_blueprint", __name__, url_prefix="/sellers")
+sellers_blueprint = Blueprint("sellers_blueprint", __name__, url_prefix="/api/sellers")
 
 from . import sellers_contoller
