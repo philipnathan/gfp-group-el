69b9dbd88383e160d7225145398c668e4d9c9517
philipnathan <philipnathan168@gmail.com>
2024-08-03 15:54:02 +0800

    reusable function

--- backend/app/controllers/common/__init__.py
+++ backend/app/controllers/common/__init__.py
@@ -1,1 +1,2 @@
 from .is_filled import is_filled
+from .get_data_and_validate import get_data_and_validate
--- backend/app/controllers/common/is_filled.py
+++ backend/app/controllers/common/is_filled.py
@@ -1,4 +1,4 @@
-def is_filled(*data):
+def is_filled(**data):
     for each_data in data:
         if not each_data:
             return False
